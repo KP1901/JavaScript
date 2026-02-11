d9259e2918a26cf10211cb67c4973d23366a264e
kiranpatodekar <patodekarkiran@gmail.com>
2026-02-11 15:47:43 +0530

    updated

--- JAVASCRIPT/pr1.js
+++ JAVASCRIPT/pr1.js
@@ -1,23 +1,6 @@
-let fibonacci = {
-  limit: 100,
-  [Symbol.iterator]() {
-    let i = 0;
-
-    return {
-      next() {
-        while (i < this.limit) {
-          if (i < this.limit) {
-            i++;
-            return { value: i, done: false };
-          } else {
-            return { value: undefined, done: true };
-          }
-        }
-      },
-    };
-  },
-};
-
-for (const item of fibonacci) {
-  console.log(item);
+function test() {
+  console.log(x);
 }
+
+let x = 50;
+test();
