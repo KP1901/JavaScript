e801836d1d1d5b9c80453b9509459621c5cdbce4
kiranpatodekar <patodekarkiran@gmail.com>
2026-03-02 17:11:02 +0530

    updated

---
+++ JAVASCRIPT/Project/new/shopping-cart/script.js
@@ -0,0 +1,125 @@
+const products = [
+  {
+    id: 1,
+    name: "laptop",
+    price: 50000,
+  },
+  {
+    id: 2,
+    name: "Mouse",
+    price: 500,
+  },
+  {
+    id: 3,
+    name: "Keyboard",
+    price: 1000,
+  },
+];
+
+const carts = [];
+
+const productList = document.querySelector(".product-list");
+const cartList = document.querySelector(".cart-list");
+
+// const view = {
+//   productList: document.querySelector(".product-list"),
+//   product: productList.querySelector(".product"),
+
+// const controller = {
+//   init() {
+//     view.product.addEventListener("click", (e) => {
+//       console.log(e.target);
+//     });
+
+//
+//   },
+// };
+// controller.init();
+
+function createProductElement(product) {
+  let productEl = document.createElement("div");
+  productEl.className = "product";
+  productEl.dataset.id = product.id;
+
+  let productInfo = document.createElement("div");
+  productInfo.className = "product-info";
+
+  let productNameEl = document.createElement("h3");
+  productNameEl.className = "product-name";
+  productNameEl.textContent = product.name;
+
+  let productPriceEl = document.createElement("p");
+  productPriceEl.className = "product-price";
+  productPriceEl.textContent = `₹${product.price}`;
+
+  let cartButton = document.createElement("button");
+  cartButton.className = "cart-button";
+  cartButton.textContent = "Add to Cart";
+  productInfo.append(productNameEl, productPriceEl);
+  productEl.append(productInfo, cartButton);
+  productList.append(productEl);
+}
+
+products.forEach((product) => {
+  createProductElement(product);
+});
+
+productList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("cart-button")) {
+    let product = e.target.closest(".product");
+    let id = Number(product.dataset.id);
+
+    let productObject = products.find((product) => product.id === id);
+
+    let cartProduct = carts.find((product) => product.id === id);
+
+    if (cartProduct) {
+      cartProduct.quantity++;
+      renderCart();
+    } else {
+      let newCartProduct = {
+        ...productObject,
+        quantity: 1,
+      };
+      carts.push(newCartProduct);
+      renderCart();
+    }
+  }
+});
+function renderCart() {
+  cartList.innerHTML = "";
+
+  carts.forEach((cartProduct) => {
+    createCartElement(cartProduct);
+  });
+}
+
+function createCartElement(cartProduct) {
+  const cartProductElement = document.createElement("div");
+
+  const cartProductTop = document.createElement("div");
+
+  const productNameEl = document.createElement("div");
+
+  const productPriceEl = document.createElement("div");
+
+  const removeButtonEl = document.createElement("button");
+
+  const qtySectionEl = document.createElement("div");
+
+  const qtyIncrementBtnEl = document.createElement("button");
+
+  const quantityEl = document.createElement("span");
+
+  const qtyDecrementBtnEl = document.createElement("button");
+
+  const subTotal = document.createElement("div");
+}
+
+/*
+IMP Notes:
+
+1.if you create elements dynamically then use event delegation
+2.if state changes then first clear ui in render then creates the elements
+
+*/
