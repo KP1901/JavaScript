66e5aee757523cbabd78ae8fc1bf613981afec4f
kiranpatodekar <patodekarkiran@gmail.com>
2026-05-24 13:03:55 +0530

    updated

--- JAVASCRIPT/Project/01_WeatherApp/script.js
+++ JAVASCRIPT/Project/01_WeatherApp/script.js
@@ -7,27 +7,53 @@ const temperatureEl = document.querySelector(".temperature");
 const humidityEl = document.querySelector(".humidity");
 const windEl = document.querySelector(".wind");
 const cityEl = document.querySelector(".city");
+const weatherHideContainer = document.querySelector(".weather-hide-container");
+const message = document.getElementById("message");
+
+weatherHideContainer.style.display = "none";
 
 const apiKey = "a6446d4c283dd861e397865d928b3276";
 
 https: weatherInputFormEl.addEventListener("submit", (e) => {
   e.preventDefault();
   let cityName = cityInput.value;
+
+  if (!cityName) {
+    message.style.display = "block";
+    message.textContent = "Enter City Name....";
+    weatherHideContainer.style.display = "none";
+    return;
+  }
+
   getWeatherData(cityName);
+  cityInput.value = "";
 });
 
 async function getWeatherData(cityName) {
-  const weatherDataRes = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
-  );
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
+    );
 
-  const weatherData = await weatherDataRes.json();
+    if (!response.ok) {
+      message.style.display = "block";
+      message.textContent = "Invalid City Name";
+      weatherHideContainer.style.display = "none";
 
-  console.log(weatherData);
+      throw new Error("HTTP Error", response.status);
+    }
 
-  weatherImg.src = getAssetPath(weatherData.weather[0].main);
-  temperatureEl.textContent = `${weatherData.main.temp}C`;
-  humidityEl.textContent = `${weatherData.main.humidity}C`;
-  cityEl.textContent = weatherData.name;
-  windEl.textContent = `${weatherData.wind.speed} km/hr`;
+    weatherHideContainer.style.display = "block";
+    message.style.display = "none";
+    message.textContent = "";
+    const weatherData = await response.json();
+
+    weatherImg.src = getAssetPath(weatherData.weather[0].main);
+    temperatureEl.textContent = `${Math.ceil(weatherData.main.temp)}°C`;
+    humidityEl.textContent = `${weatherData.main.humidity}%`;
+    cityEl.textContent = weatherData.name;
+    windEl.textContent = `${weatherData.wind.speed} km/hr`;
+  } catch (error) {
+    console.log(error);
+  }
 }
