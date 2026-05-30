11fa0079a95977eed277a348deefaaa5ae7b75b9
kiranpatodekar <patodekarkiran@gmail.com>
2026-05-30 13:19:16 +0530

    updated

--- JAVASCRIPT/Project/02_To-Do List/script.js
+++ JAVASCRIPT/Project/02_To-Do List/script.js
@@ -5,20 +5,24 @@ const todoLists = document.querySelector(".todo-lists");
 let lists = JSON.parse(localStorage.getItem("tasks")) || [];
 
 function runTodoApp() {
-  addBtn.onclick = (e) => {
-    let inputText = taskInput.value;
+  addBtn.onclick = () => {
+    let inputText = taskInput.value.trim();
+
+    if (!inputText) return;
 
     let listObject = {
-      id: crypto.randomUUID().slice(0, 4),
+      id: crypto.randomUUID(),
       text: inputText,
       checked: false,
     };
 
-    createListElement(inputText, listObject.id);
+    createListElement(listObject.text, listObject.id, listObject.checked);
 
     lists.push(listObject);
 
-    saveListObject(listObject);
+    saveListObject();
+
+    taskInput.value = "";
   };
 
   todoLists.addEventListener("click", (e) => {
@@ -36,46 +40,54 @@ function runTodoApp() {
         toggleEL.classList.add("line-through");
         imageEl.src = "./assets/multiply.png";
       }
-      let listEl = e.target.closest(".list");
 
-      console.log(lists);
+      let listEl = e.target.closest(".list");
 
       let item = lists.find((list) => list.id === listEl.dataset.id);
 
-      if (item.checked) {
-        item.checked = false;
-      } else {
-        item.checked = true;
+      if (item) {
+        item.checked = !item.checked;
       }
+
       saveListObject();
-    } else {
-      deleteTodo();
+    } else if (e.target.classList.contains("delete-btn")) {
+      let listEl = e.target.closest(".list");
+
+      deleteTodo(listEl.dataset.id, listEl);
     }
   });
 
   function render(lists) {
-    lists.forEach((element) => createListElement(element.text, element.id));
+    lists.forEach((element) => {
+      createListElement(element.text, element.id, element.checked);
+    });
   }
 
   function saveListObject() {
     localStorage.setItem("tasks", JSON.stringify(lists));
   }
 
-  function deleteTodo() {
-    const updatedLists = lists.filter((list) => list.checked !== false);
+  function deleteTodo(id, listEl) {
+    let beforeLength = lists.length;
 
-    console.log(updatedLists);
+    lists = lists.filter((list) => !(list.id === id && list.checked));
+
+    if (beforeLength !== lists.length) {
+      listEl.remove();
+    }
+
+    saveListObject();
   }
 
-  function createListElement(inputText, id) {
+  function createListElement(inputText, id, checked) {
     const listEl = document.createElement("div");
     listEl.className = "list";
     listEl.dataset.id = id;
 
     const imgEl = document.createElement("img");
-    imgEl.src = "./assets/unchecked.png";
-    imgEl.width = "25";
-    imgEl.alt = "uncheck image";
+    imgEl.src = checked ? "./assets/multiply.png" : "./assets/unchecked.png";
+
+    imgEl.width = 25;
     imgEl.className = "img";
 
     const textEl = document.createElement("p");
@@ -89,10 +101,18 @@ function runTodoApp() {
     const toggleDivEl = document.createElement("div");
     toggleDivEl.className = "toggle";
 
+    if (checked) {
+      toggleDivEl.classList.add("line-through");
+    }
+
     toggleDivEl.append(imgEl, textEl);
+
     listEl.append(toggleDivEl, btnEl);
+
     todoLists.append(listEl);
   }
+
   render(lists);
 }
+
 runTodoApp();
