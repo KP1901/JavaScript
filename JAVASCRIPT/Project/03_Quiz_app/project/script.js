b843fb8f45d6bde2208e23f32ba8ff2dab8dd55b
kiranpatodekar <patodekarkiran@gmail.com>
2026-06-06 12:38:48 +0530

    updated

--- JAVASCRIPT/Project/03_Quiz_app/project/script.js
+++ JAVASCRIPT/Project/03_Quiz_app/project/script.js
@@ -3,11 +3,15 @@ import { quizData } from "./quizData.js";
 const nextBtn = document.querySelector(".nextBtn");
 const questionEl = document.querySelector(".question");
 const answersBtn = document.querySelectorAll(".answer");
+const scoreEl = document.querySelector(".score");
+const containerEl = document.querySelector(".container");
 
 let currentQuestionIndex = 0;
+let score = 0;
 
 function showQuestion() {
-  questionEl.textContent = `${currentQuestionIndex + 1} ${quizData[currentQuestionIndex].question}`;
+  questionEl.textContent = `${currentQuestionIndex + 1}. ${quizData[currentQuestionIndex].question}`;
+  console.log(currentQuestionIndex);
 
   answersBtn.forEach((answerBtn, index) => {
     answerBtn.textContent = quizData[currentQuestionIndex].answers[index];
@@ -15,13 +19,18 @@ function showQuestion() {
     answerBtn.classList.remove("correct");
     answerBtn.classList.remove("incorrect");
   });
+
+  nextBtn.disabled = true;
 }
 
 answersBtn.forEach((answerBtn, index) => {
   answerBtn.addEventListener("click", (e) => {
+    nextBtn.disabled = false;
+
     let answer = e.target.textContent;
     if (answer === quizData[currentQuestionIndex].correct) {
       e.target.classList.add("correct");
+      score++;
       answersBtn.forEach((answerBtn) => {
         answerBtn.disabled = true;
       });
@@ -37,14 +46,47 @@ answersBtn.forEach((answerBtn, index) => {
   });
 });
 
-if (currentQuestionIndex <= quizData.length - 1) {
-  nextBtn.addEventListener("click", () => {
-    currentQuestionIndex++;
-    showQuestion();
-    console.log(currentQuestionIndex);
-  });
-} else {
+function showScore() {
+  scoreEl.textContent = `${score} / ${quizData.length} are True`;
   nextBtn.disabled = true;
 }
 
-showQuestion();
+nextBtn.addEventListener("click", () => {
+  currentQuestionIndex++;
+  if (nextBtn.textContent == "Next") {
+    if (currentQuestionIndex < quizData.length) {
+      showQuestion();
+    } else {
+      runApp();
+      containerEl.innerHTML = "";
+      nextBtn.textContent = "Restart";
+      nextBtn.disabled = false;
+    }
+  } else {
+    currentQuestionIndex = 0;
+    showQuestion();
+    // nextBtn.textContent = "Next";
+    nextBtn.disabled = false;
+  }
+});
+
+// nextBtn.addEventListener("click", () => {
+//   nextBtn.textContent = "Next";
+//   nextBtn.disabled = false;
+// });
+
+function runApp() {
+  showQuestion();
+}
+
+runApp();
+
+/* 
+0 -> 1  so 1 < 6
+1 -> 2  so 2 < 6
+2 -> 3  so 3 < 6
+3 -> 4  so 4 < 6
+4 -> 5  so 5 < 6
+5 -> 6  so 6 < 6
+
+*/
